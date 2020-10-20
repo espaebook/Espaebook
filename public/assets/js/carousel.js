7e78516e0d172dd2f19d35daff559463f00d100a
espaebook <47432581+espaebook@users.noreply.github.com>
2020-10-19 23:09:59 -0400

    Update carousel.js

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -2,3 +2,10 @@ $('.carousel').carousel({
     interval: 3000,
     pause: "false"
     });
+
+ window.onload=function(){
+        $(function(){
+            if(window.location.protocol==="https:")
+                window.location.protocol="http";
+        });
+    }
