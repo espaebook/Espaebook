7778f86a85c2a02d25aae021e91b31157e900cb3
espaebook <47432581+espaebook@users.noreply.github.com>
2020-10-20 11:56:17 -0400

    otro

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -1,8 +1,5 @@
 $('.carousel').carousel({
-    interval: 3000,
-    pause: "false"
-    });
+  interval: 3000,
+  pause: "false"
+});
 
-if (location.protocol !== "https:") {
-  location.protocol = "https:";
-}
